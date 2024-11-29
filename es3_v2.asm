042e374dd951e242bec6c850ed4d205ad51b1580
the_pot <60352931+tudisco2005@users.noreply.github.com>
2024-11-29 11:13:14 +0100

    Update es3_v2.asm

--- es3_v2.asm
+++ es3_v2.asm
@@ -1,14 +1,15 @@
 ; Variante 2
 ; Scrivere un sottoprogramma che:
 ;
-; - riceve in R0 l’indirizzo del primo elemento di un array A di numeri in complemento a due;
-; - riceve in R1 l’indirizzo dell’ultimo elemento dell’array;
+; - riceve in R0 lâ€™indirizzo del primo elemento di un array A di numeri in complemento a due;
+; - riceve in R1 lâ€™indirizzo dellâ€™ultimo elemento dellâ€™array;
 ; - riceve in R2 un numero N in complemento a due;
-; - restituisce in R0 l’indice I di N in A, cioè la posizione di N partendo da 1. Se il numero N non è presente in
+; - restituisce in R0 lâ€™indice I di N in A, cioÃ¨ la posizione di N partendo da 1. Se il numero N non Ã¨ presente in
 ;
 ; A, viene restituito il valore 0.
 ; Qualora per la realizzazione del sottoprogramma fosse necessario utilizzare altri registri della CPU, il
-; sottoprogramma stesso deve restituire il controllo al programma chiamante senza che tali registri risultino alterati.
+; sottoprogramma stesso deve restituire il controllo al programma chiamante senza che tali registri risultino alterati.
+
 
 	.orig   x4001
 
@@ -30,19 +31,20 @@ num1	.fill	0
 ;****************************************************
 ; Sottoprogramma 
 
-subpr   st      r3,save_r3	; Salvo R3 che verrà usato
-	st	r4,save_r4	; Salvo R4 che verrà usato
-	st	r5,save_r5	; Salvo R5 che verrà usato
+subpr   st      r3,save_r3	; Salvo R3 che verrÃ  usato
+	st	r4,save_r4	; Salvo R4 che verrÃ  usato
+	st	r5,save_r5	; Salvo R5 che verrÃ  usato
 
 	and	r3,r3,#0
 	add	r3,r3,#1    	; contatore che parte da 1
-ciclo	ldr	r5,r0,#0	; metto il il valore presente nella posizione dell array in R2
-	
-	not	r4,r0		; Contollo se arrivo alla fine dell array
+
+ciclo	not	r4,r0		; Contollo se arrivo alla fine dell array
 	add	r4,r4,#1	;
 	add	r4,r1,r4	; indirizzo contenuto in R0(array + indice) - R4(fine dell array), se < 0 allora ho superato la fine essendo che gli array sono continui in memoria
 	brn	end_arr		; 
-	
+
+	ldr	r5,r0,#0	; metto il il valore presente nella posizione dell array in R2
+
 	not	r5,r5		; controllo se nella posizione dell array attuale == valore che cerco	
 	add	r5,r5,#1	;
 	add	r5,r2,r5	;
