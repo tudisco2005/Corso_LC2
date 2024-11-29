06244100b3909af15ddae05681ec0f138ecb9781
the_pot <60352931+tudisco2005@users.noreply.github.com>
2024-11-29 09:07:14 +0100

    Update es2.asm

--- es2.asm
+++ es2.asm
@@ -3,13 +3,13 @@
 ;Scrivere un sottoprogramma che riceve in R0 e R1 due numeri num1 e num2 in complemento a due, li somma,
 ;restituisce in R1 il risultato e in R0 la seguente indicazione:
 
-;R0 = -1 se si è verificato underflow
+;R0 = -1 se si Ã¨ verificato underflow
 ;R0 = 0 se la somma ha avuto esito corretto
-;R0 = +1 se si è verificato overflow
+;R0 = +1 se si Ã¨ verificato overflow
 
 ;Qualora per la realizzazione del sottoprogramma fosse necessario utilizzare altri registri della CPU, il
 ;sottoprogramma stesso deve restituire il controllo al programma chiamante senza che tali registri risultino
-;alterati.
+;alterati
 
 	.orig   x4001
     	
@@ -18,14 +18,29 @@
     	ld      r1,num2     	; Carico il secondo numero in R1
     	jsr     subpr       	; Chiamata al sottoprogramma
 
+	ld      r0,num3     	
+    	ld      r1,num4     	
+    	jsr     subpr
+	
+	ld      r0,num5    	
+    	ld      r1,num6     	
+    	jsr     subpr
+
 stoqui	brnzp	stoqui
-num1    .fill   32767
-num2    .fill   10
+
+num1    .fill   32765 		; somma senza overflow o underflow
+num2    .fill   -10
+
+num3   .fill   -32765 		; mi aspetto un underflow
+num4    .fill   -10
+
+num5    .fill   32765 		; mi aspetto un overflow
+num6    .fill   10
 
 ;****************************************************
 ; Sottoprogramma per somma con gestione overflow/underflow
 subpr   
-	st      r2,save_r2  	; Salvo R2 che verrà usato
+	st      r2,save_r2  	; Salvo R2 che verrÃ  usato
     
     	; Eseguo la somma
     	add     r2,r0,r1    	; Sommo num1 + num2 in R2
@@ -37,11 +52,11 @@ subpr
     	; Caso num1 negativo
     	and     r1,r1,r1    	; Test segno num2
     	brn     neg_check   	; Se anche num2 negativo, vai a neg_check
-    	brnzp   no_ovf      	; Se num2 positivo, non c'è overflow
+    	brnzp   no_ovf      	; Se num2 positivo, non c'Ã¨ overflow
     
 pos_check
     	and     r1,r1,r1	; Test segno num2
-    	brn     no_ovf     	; Se num2 negativo, non c'è overflow
+    	brn     no_ovf     	; Se num2 negativo, non c'Ã¨ overflow
     	and     r2,r2,r2   	; Test segno risultato
     	brn     overflow	; Se risultato negativo e operandi positivi -> underflow
     	brnzp   no_ovf
