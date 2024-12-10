bc262dc1790f229b7e60067cfc55b414a1c9d4a2
the_pot <60352931+tudisco2005@users.noreply.github.com>
2024-12-10 23:17:12 +0100

    Update es4_04-11-2024.asm

--- es4_04-11-2024.asm
+++ es4_04-11-2024.asm
@@ -1,7 +1,7 @@
 	.orig	x4001
 	lea	r0,arr
 	ld	r1,num1
-	jsr	OR_ARRAY
+	jsr	OR_ARR
 	
 stoqui	brnzp 	stoqui
 
@@ -10,17 +10,17 @@ arr 	.fill	b0000111100001111
 	.fill	b1100110011001100
 	.fill	b0000000000000000
 
-num1 	.fill 	1111 0000 1111 0000
+num1 	.fill 	b1111000011110000
 ;**********************************
-OR_ARRAY
+OR_ARR
 	st	r2,save_r2
 	st	r3,save_r3
 
+	not	r1,r1
 ciclo	ldr	r2,r0,#0
 	brz	fine
 	
 	not 	r2,r2
-	not	r1,r1
 	and	r3,r1,r2
 	not	r3,r3
 	str	r3,r0,#0
@@ -31,7 +31,6 @@ incr	add	r0,r0,#1
 fine	ld	r2,save_r2
 	ld	r3,save_r3
 	ret
-
 
 save_r2	.blkw	1
 save_r3	.blkw	1
